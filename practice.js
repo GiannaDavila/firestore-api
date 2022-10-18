eb1e29770f8466b0096f9df18f1291cc23f0babc
GiannaDavila <giannagiannadavila@gmail.com>
2022-10-18 13:41:31 -0400

    first commit

---
+++ practice.js
@@ -0,0 +1,14 @@
+const studentsCanDrink= student.filter (stud=> stud.age>=21)[
+    {
+        name: "gianna",
+        age :18,
+        gender: "female" 
+    },{
+        name: "beboo",
+        age:25,
+        gneder: "male "
+    },{
+
+    }
+]
+arrays, objects, loops, functions, variables 
---
+++ src/coffees.js
@@ -0,0 +1,17 @@
+import dbConnect from "./dbConnect.js";
+
+export function createCoffee(req,res){
+    const db = dbConnect()
+    db.collection('coffees').add(req.body)
+    .then(doc => res.status(201).send({ success: true, message: 'Created coffee:' + doc.id}))
+    .catch(err => res.status(500).send({ success: false, message: err }))
+}
+export function getAllCoffees(req,res){
+    const db = dbConnect()
+    db.collection('coffees').get()
+    .then(collection => {
+        const coffeeList = collection.docs.map(doc => doc.data())
+        res.send(coffeeList)
+    })
+    .catch(err => res.status(500).send({ success: false, message: err }))
+}
